b5d26efdde8d19f596884955b156d4965718813a
Charles Dusek <dusek@onomy.io>
2022-06-19 23:47:40 -0500

    Added in json body

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,14 @@ fn main() {
     println!("Hello, world!");
 
     let mut keys_values = Map::new();
+    let mut body = Map::new();
+
 
     let l = 4;
     let m = 4;
     let n = 4;
+
+    let nonce = 1;
 
     let mut rng = rand::thread_rng();
     println!("Integer from range n: {}", rng.gen_range(0, m));
@@ -28,16 +32,20 @@ fn main() {
 
     println!("{}", serde_json::to_string_pretty(&keys_values).unwrap());
 
-    let mut csprng = OsRng{};
+    let csprng = OsRng{};
     let keypair: Keypair = Keypair::generate(&mut OsRng);
     let public_key: PublicKey = keypair.public;
     let public_key_bytes: [u8; PUBLIC_KEY_LENGTH] = public_key.to_bytes();
+    let public_key_json = json!(public_key_bytes);
+    
+    body.insert("public_key".to_string(), public_key_json);
+    body.insert("keys".to_string(), json!(keys_values));
+    body.insert("nonce".to_string(), json!(nonce));
+
+    println!("{}", serde_json::to_string_pretty(&body).unwrap());
 
     let message: &[u8] = b"This is a test of the tsunami alert system.";
     let signature: Signature = keypair.sign(message);
 
-    println!("\n\nWrite public key to json");
-    let demo_json = json!(public_key_bytes);
-    println!("{}", demo_json);
-
+    
 }
