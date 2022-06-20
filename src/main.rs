b175d815d8f2bd6686993db26d418949fe66ead8
Charles Dusek <38732970+cgdusek@users.noreply.github.com>
2022-06-20 16:15:43 -0500

    build tx in place. still need troubleshooting

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,28 @@
 use serde_json::{Map, json};
 use rand::Rng;
+use std::str;
 
 extern crate ed25519_dalek;
 
 use rand::rngs::OsRng;
 use ed25519_dalek::Keypair;
 use ed25519_dalek::{Signature, Signer, PublicKey, PUBLIC_KEY_LENGTH, SIGNATURE_LENGTH};
+use ed25519_dalek::Sha512;
+use ed25519_dalek::Digest;
 
 fn main() {
     println!("Hello, world!");
 
     let mut keys_values = Map::new();
     let mut body = Map::new();
+    let mut tx = Map::new();
 
 
     let l = 4;
     let m = 4;
     let n = 4;
 
-    let nonce = 1;
+    let nonce: u64 = 1;
 
     let mut rng = rand::thread_rng();
     println!("Integer from range n: {}", rng.gen_range(0, m));
@@ -44,8 +48,24 @@ fn main() {
 
     println!("{}", serde_json::to_string_pretty(&body).unwrap());
 
-    let message: &[u8] = b"This is a test of the tsunami alert system.";
-    let signature: Signature = keypair.sign(message);
+    let message = serde_json::to_string(&body).unwrap();
 
+    // Create a hash digest object which we'll feed the message into:
+    let mut prehashed: Sha512 = Sha512::new();
+
+    prehashed.update(message);
+
+    let context: &[u8] = b"onomy-rs_transaction";
+
+    let signature: Signature = keypair.sign_prehashed(prehashed.clone(), Some(context)).unwrap();
+
+    let signature_bytes: [u8; SIGNATURE_LENGTH] = signature.to_bytes();
+    let prehashed_bytes = prehashed.finalize();
+
+    tx.insert("hash".to_string(), json!(str::from_utf8(&prehashed_bytes).unwrap()));
+    tx.insert("body".to_string(), json!(body));
+    tx.insert("signature".to_string(), json!(str::from_utf8(&signature_bytes).unwrap()));
+
+    println!("{}", serde_json::to_string_pretty(&tx).unwrap());
     
 }
